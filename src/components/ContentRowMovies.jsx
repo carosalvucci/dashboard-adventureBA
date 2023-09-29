3c1bbc6dd52bb9b78b5d80ff4854c94d89f60690
Caro Salvucci <115897236+carosalvucci@users.noreply.github.com>
2023-09-29 17:53:18 -0300

    Add files via upload

---
+++ src/components/ContentRowMovies.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Cards } from './index'
+
+
+export default function ContentRowMovies({usersCount, productInfo}) {
+  return (
+      <div className='row'>
+          <Cards 
+            title = "Users total"
+            quantity = {usersCount}
+            color = "primary"
+            icon = "fa-film" />
+          <Cards 
+            title = "Products total"
+            quantity = {productInfo.count}
+            color = "success"
+            icon = "fa-award" />
+          
+      </div>
+  )
+}
+
